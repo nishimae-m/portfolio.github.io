29a14cba98b98bbf37895c30fb149fe8fedb50c6
2311-R03-N-1379\nishimae_marie <nishimae.1275@universal-osaka.com>
2025-04-01 20:00:57 +0900

    Initial commit

---
+++ static/js/script.js
@@ -0,0 +1,14 @@
+$(function () {
+  var navHeight = $(".header").outerHeight();
+  $('a[href^="#"]').on("click", function (event) {
+    event.preventDefault();
+    var href = $(this).attr("href");
+    var target = $(href == "#" || href == "" ? "html" : href);
+    var position = target.offset().top - navHeight;
+    $("html, body").animate({ scrollTop: position }, 300, "swing");
+  });
+  $("#js-page-top").on("click", function () {
+    $("body,html").animate({ scrollTop: 0 }, 300);
+    return false;
+  });
+});
