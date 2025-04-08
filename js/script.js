129ce03b307223ae86ce9752454bff8f5fe49d34
2311-R03-N-1379\nishimae_marie <nishimae.1275@universal-osaka.com>
2025-04-08 10:34:05 +0900

    Merge remote-tracking branch 'origin/main' # Please enter a commit message to explain why this merge is necessary, # especially if it merges an updated upstream into a topic branch. # # Lines starting with '#' will be ignored, and an empty message aborts # the commit.

--- js/script.js
+++ js/script.js
@@ -7,8 +7,4 @@ $(function () {
     var position = target.offset().top - navHeight;
     $("html, body").animate({ scrollTop: position }, 300, "swing");
   });
-  $("#js-page-top").on("click", function () {
-    $("body,html").animate({ scrollTop: 0 }, 300);
-    return false;
-  });
 });
